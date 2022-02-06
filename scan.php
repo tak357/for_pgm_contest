9009340ad44ba5907e94d8a2ec50e1c48cfb8d08
take <52248763+tak357@users.noreply.github.com>
2022-02-06 18:48:12 +0900

    Update scan.php

--- scan.php
+++ scan.php
@@ -2,13 +2,12 @@
 $arr = [];
 $str = trim(fgets(STDIN));
 $arr = explode(' ', $str);
-
 class Scanner {
     private $arr = [];
     private $count = 0;
     private $pointer = 0;
     public function next() {
-        if($this->pointer >= $this->count) {
+        if ($this->pointer >= $this->count) {
             $str = trim(fgets(STDIN));
             $this->arr = explode(' ', $str);
             $this->count = count($this->arr);
@@ -21,6 +20,10 @@ class Scanner {
     public function hasNext() { return $this->pointer < $this->count; }
     public function nextInt() { return (int)$this->next(); }
     public function nextDouble() { return (double)$this->next(); }
+    public function nextString() { return (string)$this->next(); }
+    public function int2keta($a, $b) { return ($a * 10 + $b); }
+    public function int3keta($a, $b, $c) { return ($a * 100 + $b * 10 + $c); }
+    public function int4keta($a, $b, $c, $d) { return ($a * 1000 + $b * 100 + $c * 10 + $d); }
 }
 class out {
     public static function println($str = "") { echo $str . PHP_EOL; }
@@ -29,4 +32,3 @@ class out {
 // $sc = new Scanner();
 // $n = $sc->nextInt();
 // out::println($n);
-
